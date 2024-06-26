fde343a928f8db78c23963f9b0b4875f99d07616
miral2525 <142686773+miral2525@users.noreply.github.com>
2024-06-26 15:06:39 +0530

    Add files via upload

---
+++ project qrcode 11.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data;
+using System.IO;
+using project11.Controllers;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace project11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MiralDBEntities db = new MiralDBEntities();
+
+
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult LoginA(LOGIN login)
+        {
+            try
+            {
+                var data = db.LOGINs.FirstOrDefault(x => x.EMAIL == login.EMAIL && x.PASSWORD == login.PASSWORD);
+                if (data != null)
+                {
+                    return Json(new { success = true, message = "Login successfull" });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Invalid Username or password" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Title = "Signup";
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Signup(LOGIN login)
+        {
+            try
+            {
+                db.LOGINs.Add(login);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Signup successful" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(ADDRESS_BOOK addressBook)
+        {
+            try
+            {
+                db.ADDRESS_BOOK.Add(addressBook);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Data saved successfully" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+    }
+}
